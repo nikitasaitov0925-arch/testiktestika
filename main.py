0489fdd79774a7a8f732c1987223a189758b803f
nikitasaitov0925-arch <nikitasaitov0925@gmail.com>
2026-08-23 17:12:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -859,6 +859,8 @@ async def reset_top(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # ===== ОБРАБОТЧИКИ ТЕКСТА И ДОКУМЕНТОВ =====
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    rp_data = load_rp_responses()
+    print(f"📦 Содержимое rp_data: {rp_data}")  # ← ПОКАЖЕТ ВСЁ СОДЕРЖИМОЕ
      # ===== СНАЧАЛА ПРОВЕРЯЕМ RP =====
     text = update.message.text.strip().lower()
     user_name = update.effective_user.first_name
